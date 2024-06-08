0fb9edf78504e762ffcc267345c9690542f78034
huynhtrong007 <huynhtrongz@gmail.com>
2024-06-08 21:41:14 +0700

    Tạo module brand,tạo giao diện, chức năng thêm,xóa sửa

--- shop_clothes/routes/web.php
+++ shop_clothes/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\BrandController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,12 +20,22 @@ Route::get('/', function () {
 });
 
 
-// Admin template
+/**============Admin============ */
+
+/** module user* */
 Route::get('/admin-login', [UserController::class, 'login']);
 Route::get('/admin-user', [UserController::class, 'index']);
-Route::get('/admin-user-create', [UserController::class, 'create']);
+Route::post('/admin-user-create', [UserController::class, 'create']);
 Route::get('/admin-user-edit/{id}', [UserController::class, 'edit']);
 Route::get('/admin-user-show/{id}', [UserController::class, 'show']);
+/** module brand* */
+
+Route::get('/admin-brand', [BrandController::class, 'index'])->name('brand.index');
+Route::get('/admin-brand-create', [BrandController::class, 'create'])->name('brand.create');
+Route::post('/admin-brand-store', [BrandController::class, 'store'])->name('store.brand');
+Route::get('/admin-brand-edit/{id}', [BrandController::class, 'edit'])->name('edit.brand');
+Route::get('/admin-brand-destroy/{id}', [BrandController::class, 'destroy'])->name('destroy.brand');
+
 
 
 
