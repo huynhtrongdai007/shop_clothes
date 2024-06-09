1536a47d3f184a2751b70f4231dbfe8c6bf965cb
huynhtrong007 <huynhtrongz@gmail.com>
2024-06-09 21:15:06 +0700

    cài đặt thư viện jetstream

--- shop_clothes/routes/web.php
+++ shop_clothes/routes/web.php
@@ -39,3 +39,13 @@ Route::get('/admin-brand-destroy/{id}', [BrandController::class, 'destroy'])->na
 
 
 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
